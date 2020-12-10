927e5e9402a64944345dbd67f4692318e3c568b9
Sahil Khurana <47140401+sahil1049@users.noreply.github.com>
2020-12-10 13:52:11 +0530

    Create finder.py

---
+++ finder.py
@@ -0,0 +1,29 @@
+
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+
+file1 = open('read.txt', 'r') 
+
+Lines = file1.readlines()
+
+for i in Lines:
+
+
+ 
+ req = Request("https"+i)
+ print("Status of "+i)
+ try:
+    response = urlopen(req)
+ except HTTPError as e:
+    print('The server couldn\'t fulfill the request.')
+    print('Error code: ', e.code)
+ except URLError as e:
+    file2=open('write.txt','a')
+    print('We failed to reach a server.')
+    file2.write("http://"+i)  
+    file2.close()
+    print('Reason: ', e.reason)
+    
+ else:
+    
+    print ('Website is working fine')
